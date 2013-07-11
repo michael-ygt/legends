61164ef932863e717b78745bba6806fa6eddf129
michael-ygt <michael.cavanagh@yourgolftravel.com>
2013-07-11 16:21:49 +0100

    Heh

--- Gemfile
+++ Gemfile
@@ -5,3 +5,8 @@ gem "middleman", "~>3.1.3"
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
 
+# Deploy a middleman built site over rsync, ftp, or git
+gem "middleman-deploy", "~> 0.0.12"
+
+# Minify HTML
+gem "middleman-minify-html"
--- config.rb
+++ config.rb
@@ -7,9 +7,10 @@
 # require 'susy'
 
 # Change Compass configuration
-compass_config do |config|
-	config.output_style = :compact
-end
+# compass_config do |config|
+# 	config.output_style = :compact
+# 	config.sass_options = {:debug_info => true}
+# end
 
 #####################
 # Page options, layouts, aliases and proxies
@@ -25,12 +26,12 @@ end
 #
 # A path which all have the same layout
 # with_layout :admin do
-#   page "/admin/*"
+# 	page "/admin/*"
 # end
 
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# 	:which_fake_page => "Rendering a fake page with a local variable" }
 
 #####################
 # Helpers
@@ -42,11 +43,19 @@ end
 # Reload the browser automatically whenever files change
 activate :livereload
 
+# Enable automatic deployment to GitHub Pages
+# See https://github.com/tvaughan/middleman-deploy
+activate :deploy do |deploy|
+	deploy.method = :git
+	# deploy.after_build = true
+	deploy.clean = true
+end
+
 # Methods defined in the helpers block are available in templates
 # helpers do
-#   def some_helper
-#     "Helping"
-#   end
+# 	def some_helper
+# 		"Helping"
+# 	end
 # end
 
 #####################
@@ -57,25 +66,33 @@ set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'img'
 
+activate :directory_indexes
+
 # Build-specific configuration
 configure :build do
 	# For example, change the Compass output style for deployment
 	activate :minify_css
 
 	# Minify Javascript on build
-  # activate :sprockets
+	# activate :sprockets
 	activate :minify_javascript
+
+	# Minify HTML
+	# activate :minify_html
 
 	# Enable cache buster
 	# activate :asset_hash
 
+	# Enable GZIP compression
+	# activate :gzip
+
 	# Use relative URLs
 	activate :relative_assets
 
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+	# Compress PNGs after build
+	# First: gem install middleman-smusher
+	# require "middleman-smusher"
+	# activate :smusher
 
 	# Use a different image path
 	# set :http_path, "/Content/images/"
